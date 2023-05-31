9d2f56674a01fa021f4a13887704a664e0da888a
gosmall100 <45118070+gosmall100@users.noreply.github.com>
2023-05-31 17:15:46 +0800

    Update storage.go

--- storage.go
+++ storage.go
@@ -1,1 +1,215 @@
+package gofastdfsclient
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"net"
+	"os"
+)
+
+// upload task
+type storageUploadTask struct {
+	header
+	fileInfo         *fileInfo
+	storagePathIndex int8
+	fileId           string
+}
+
+func (s *storageUploadTask) SendReq(conn net.Conn) error {
+	s.cmd = STORAGE_PROTO_CMD_UPLOAD_FILE
+	s.pkgLen = s.fileInfo.fileSize + 15
+
+	if err := s.SendHeader(conn); err != nil {
+		return err
+	}
+	buffer := new(bytes.Buffer)
+	buffer.WriteByte(byte(s.storagePathIndex))
+	if err := binary.Write(buffer, binary.BigEndian, s.fileInfo.fileSize); err != nil {
+		return err
+	}
+
+	byteFileExtName := []byte(s.fileInfo.fileExtName)
+	var bufferFileExtName [6]byte
+	for i := 0; i < len(byteFileExtName); i++ {
+		bufferFileExtName[i] = byteFileExtName[i]
+	}
+	buffer.Write(bufferFileExtName[:])
+
+	if _, err := conn.Write(buffer.Bytes()); err != nil {
+		return err
+	}
+
+	var err error
+	//send file
+	if s.fileInfo.file != nil {
+		_, err = conn.(pConn).Conn.(*net.TCPConn).ReadFrom(s.fileInfo.file)
+	} else {
+		_, err = conn.Write(s.fileInfo.buffer)
+	}
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *storageUploadTask) RecvRes(conn net.Conn) error {
+	if err := s.RecvHeader(conn); err != nil {
+		return err
+	}
+
+	if s.pkgLen <= 16 {
+		return fmt.Errorf("recv file id pkgLen <= FDFS_GROUP_NAME_MAX_LEN")
+	}
+	if s.pkgLen > 100 {
+		return fmt.Errorf("recv file id pkgLen > 100,can't be so long")
+	}
+
+	buf := make([]byte, s.pkgLen)
+	if _, err := conn.Read(buf); err != nil {
+		return err
+	}
+
+	buffer := bytes.NewBuffer(buf)
+	groupName, err := readCStrFromByteBuffer(buffer, 16)
+	if err != nil {
+		return err
+	}
+	remoteFileName, err := readCStrFromByteBuffer(buffer, int(s.pkgLen)-16)
+	if err != nil {
+		return err
+	}
+
+	s.fileId = groupName + "/" + remoteFileName
+	return nil
+}
+
+type storageDownloadTask struct {
+	header
+	groupName      string
+	remoteFilename string
+	offset         int64
+	downloadBytes  int64
+	localFilename  string
+	buffer         []byte
+}
+
+func (s *storageDownloadTask) SendReq(conn net.Conn) error {
+	// download file
+	s.cmd = STORAGE_PROTO_CMD_DOWNLOAD_FILE
+	s.pkgLen = int64(len(s.remoteFilename) + 32)
+
+	if err := s.SendHeader(conn); err != nil {
+		return err
+	}
+	buffer := new(bytes.Buffer)
+	if err := binary.Write(buffer, binary.BigEndian, s.offset); err != nil {
+		return err
+	}
+	if err := binary.Write(buffer, binary.BigEndian, s.downloadBytes); err != nil {
+		return err
+	}
+	byteGroupName := []byte(s.groupName)
+	var bufferGroupName [16]byte
+	for i := 0; i < len(byteGroupName); i++ {
+		bufferGroupName[i] = byteGroupName[i]
+	}
+	buffer.Write(bufferGroupName[:])
+	buffer.WriteString(s.remoteFilename)
+	if _, err := conn.Write(buffer.Bytes()); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *storageDownloadTask) RecvRes(conn net.Conn) error {
+	if err := s.RecvHeader(conn); err != nil {
+		return fmt.Errorf("StorageDownloadTask RecvRes %v", err)
+	}
+	if s.localFilename != "" {
+		if err := s.recvFile(conn); err != nil {
+			return fmt.Errorf("StorageDownloadTask RecvRes %v", err)
+		}
+	} else {
+		if err := s.recvBuffer(conn); err != nil {
+			return fmt.Errorf("StorageDownloadTask RecvRes %v", err)
+		}
+	}
+	return nil
+}
+
+func (s *storageDownloadTask) recvFile(conn net.Conn) error {
+	file, err := os.Create(s.localFilename)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+
+	wt := bufio.NewWriter(file)
+
+	if err := writeFromConn(conn, wt, s.pkgLen); err != nil {
+		return fmt.Errorf("StorageDownloadTask RecvFile %s", err)
+	}
+	if err := wt.Flush(); err != nil {
+		return fmt.Errorf("StorageDownloadTask RecvFile %s", err)
+	}
+	return nil
+}
+
+func (s *storageDownloadTask) recvBuffer(conn net.Conn) error {
+	var (
+		err error
+	)
+	//buffer allocate by user
+	if s.buffer != nil {
+		if int64(len(s.buffer)) < s.pkgLen {
+			return fmt.Errorf("StorageDownloadTask buffer < pkgLen can't recv")
+		}
+		if err = writeFromConnToBuffer(conn, s.buffer, s.pkgLen); err != nil {
+			return fmt.Errorf("StorageDownloadTask writeFromConnToBuffer %s", err)
+		}
+		return nil
+	}
+	wt := new(bytes.Buffer)
+
+	if err = writeFromConn(conn, wt, s.pkgLen); err != nil {
+		return fmt.Errorf("StorageDownloadTask RecvBuffer %s", err)
+	}
+	s.buffer = wt.Bytes()
+	return nil
+}
+
+type storageDeleteTask struct {
+	header
+	groupName      string
+	remoteFilename string
+}
+
+func (s *storageDeleteTask) SendReq(conn net.Conn) error {
+	s.cmd = STORAGE_PROTO_CMD_DELETE_FILE
+	s.pkgLen = int64(len(s.remoteFilename) + 16)
+
+	if err := s.SendHeader(conn); err != nil {
+		return err
+	}
+	buffer := new(bytes.Buffer)
+	byteGroupName := []byte(s.groupName)
+	var bufferGroupName [16]byte
+	for i := 0; i < len(byteGroupName); i++ {
+		bufferGroupName[i] = byteGroupName[i]
+	}
+	buffer.Write(bufferGroupName[:])
+	buffer.WriteString(s.remoteFilename)
+	if _, err := conn.Write(buffer.Bytes()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *storageDeleteTask) RecvRes(conn net.Conn) error {
+	return s.RecvHeader(conn)
+}
 
